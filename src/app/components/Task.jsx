02d068f36d883f5f73f927799cf67aa78d477707
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-07-20 06:57:21 +0700

    660612137

---
+++ src/app/components/Task.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Task({title, isDone}) {
+  return (
+    <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+        <span>{title}</span>
+        <button className="btn btn-success">{isDone ? "Undo" : "Done" }</button>
+        <button className="btn btn-danger">Delete</button>
+
+    </div>
+  );
+}
