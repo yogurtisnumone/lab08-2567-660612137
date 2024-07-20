7cad893b9f223236e9593507b73c4ffee74757e9
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-07-20 07:02:14 +0700

    660612137

--- src/app/components/Task.jsx
+++ src/app/components/Task.jsx
@@ -4,7 +4,7 @@ export default function Task({title, isDone}) {
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
         <span>{title}</span>
-        <button className="btn btn-success">{isDone ? "Undo" : "Done" }</button>
+        <button className="btn btn-success">Done</button>
         <button className="btn btn-danger">Delete</button>
 
     </div>
